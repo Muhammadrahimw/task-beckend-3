b24786fce29e628442934f4c174fb57e578768c5
Muhammadrahim <muqalla21@gmail.com>
2025-02-20 10:23:39 +0500

    fixing

--- controller/users.controller.js
+++ controller/users.controller.js
@@ -1,5 +1,5 @@
 const {read} = require("../utils/fs");
-const {signInJwt} = require("../utils/jwt.js");
+const {signInJwt} = require("../utils/jwt");
 const {CustomError, ResData} = require("../utils/res-helpers");
 
 const signIn = (req, res, next) => {
